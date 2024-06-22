71fa49bb3702ad69b4a895a8f1a3c69bb359f82d
KobrinN <nikkobrin2002@mail.ru>
2024-06-22 03:39:27 +0300

    Successful mock user and role repository

--- src/test/java/org/example/service/impl/UserServiceImplTest.java
+++ src/test/java/org/example/service/impl/UserServiceImplTest.java
@@ -5,39 +5,42 @@ import org.example.map.request.newRequest.NewUserRequest;
 import org.example.model.Product;
 import org.example.model.Role;
 import org.example.model.User;
+import org.example.repo.RoleRepo;
 import org.example.repo.UserRepo;
 import org.example.repo.impl.RoleRepoImpl;
 import org.example.repo.impl.UserRepoImpl;
 import org.example.service.UserService;
+import org.junit.Before;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
-    private static UserService userService;
-    private static final UserRepo userRepo = mock(UserRepoImpl.class);
-    private static Role role;
+    @InjectMocks
+    private static UserServiceImpl userService;
+    @Mock
+    private  UserRepo userRepo;
+    @Mock
+    private  RoleRepo roleRepo;
+    @Mock
+
+    private Role role = new Role(1L, "USER");
     private static UserRepoImpl oldInstance;
 
-    private static void setMock() {
-        try(MockedStatic<UserRepoImpl> mockUserRepo = mockStatic(UserRepoImpl.class)){
-            mockUserRepo.when(UserRepoImpl::getInstance).thenReturn(userRepo);
-            userService = UserServiceImpl.getInstance();
-        }
-    }
-
-    @BeforeAll
-    static void beforeAll() {
+    @Before
+    public void beforeAll() {
+        userRepo = mock(UserRepo.class);
+        roleRepo = mock(RoleRepo.class);
+        userService = new UserServiceImpl(userRepo, roleRepo);
     }
 
     @AfterAll
@@ -49,17 +52,11 @@ class UserServiceImplTest {
     }
     @Test
     void create() {
-        try(MockedStatic<UserRepoImpl> mockUserRepo = mockStatic(UserRepoImpl.class);
-            MockedStatic<RoleRepoImpl> mockRoleRepo = mockStatic(RoleRepoImpl.class)){
-            mockUserRepo.when(UserRepoImpl::getInstance).thenReturn(userRepo);
-            userService = new UserServiceImpl(userRepo, mockRoleRepo);
-            Long id = userService.create(new NewUserRequest("Oleg", "Olegovich"));
-            User userReturn = new User(1L, "Oleg", "Olegovich", role);
-            when(userRepo.save(any())).thenReturn(userReturn);
-            Mockito.doReturn(userReturn).when(userRepo).save(Mockito.any(User.class));
-            Assertions.assertEquals(1L, id);
-        }
-
+        User userReturn = new User(1L, "Oleg", "Olegovich", role);
+        when(userRepo.save(any())).thenReturn(userReturn);
+        when(roleRepo.findById(any())).thenReturn(Optional.ofNullable(role));
+        Long id = userService.create(new NewUserRequest("Oleg", "Olegovich"));
+        Assertions.assertEquals(1L, id);
     }
 
     @Test
